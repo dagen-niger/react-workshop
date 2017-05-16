d3917d648e4edb41a0da74c3581806e466b4f247
andrey.bodrov <dagen-niger@ya.ru>
2017-05-16 16:50:24 +0200

    withLoading

--- src/withLoading.js
+++ src/withLoading.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import { compose, branch, renderComponent } from 'recompose';
+
+const Spinner = () => <div>Loading…</div>
+
+const enhance = compose(
+  branch(
+    props => props.loading,
+    renderComponent(Spinner),
+  ),
+)
+
+export default Component => enhance(Component)
