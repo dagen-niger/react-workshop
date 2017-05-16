61d445214f0b8a3779a8b1d6ad7766d9f081c490
andrey.bodrov <dagen-niger@ya.ru>
2017-05-16 13:59:36 +0200

    lab-4

--- src/withMinLengthValidation.js
+++ src/withMinLengthValidation.js
@@ -1,1 +1,21 @@
 import React from 'react';
+
+const alert = `<span>
+  Minimum 
+  4
+   required.
+</span>`
+
+const withMinLengthValidation = (component, count) =>
+    props => {
+        const fail = String(props.value).length <= 4
+        return (
+            <div>
+                {React.createElement(component, { ...props })}
+                {fail && alert}
+            </div>
+        )
+    }
+
+
+export default withMinLengthValidation
