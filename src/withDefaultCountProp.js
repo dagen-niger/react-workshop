40ccaa46ba21b82881885cad172cd6df65e66c41
andrey.bodrov <dagen-niger@ya.ru>
2017-05-16 13:42:32 +0200

    lab-3

--- src/withDefaultCountProp.js
+++ src/withDefaultCountProp.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+const withDefaultCountProp = (component, defaultCount) =>
+    props => {
+        const count = props.count || defaultCount
+        return React.createElement(component, { count })
+    }
+
+
+export default withDefaultCountProp
