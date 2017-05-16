dc703d6c6a4bc5eed087e3951ccb9f7666a55d3e
andrey.bodrov <dagen-niger@ya.ru>
2017-05-16 13:32:45 +0200

    lab-2

--- src/withDefaultCountPropNine.js
+++ src/withDefaultCountPropNine.js
@@ -1,1 +1,8 @@
 import React from 'react';
+
+const withDefaultCountPropNine = component =>
+    props => React.createElement(component, {
+        count : props.count || 9
+    })
+
+export default withDefaultCountPropNine
